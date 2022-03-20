91b6f783aa60235d5eedcc7f67223e4513061c6f
saket085 <87834511+saket085@users.noreply.github.com>
2022-03-20 22:24:06 +0000

    Generalising the script.
    Removed my EC2 Ip Address and Key Details.

--- get_metadata.sh
+++ get_metadata.sh
@@ -1,3 +1,3 @@
-ssh -i forec2.pem ec2-user@35.177.222.184 "rm metadata.json;wget -q -O - http://169.254.169.254/latest/dynamic/instance-identity/document >> metadata.json;exit"
-scp -i forec2.pem ec2-user@35.177.222.184:metadata.json .
+ssh -i path-to-privatekey.pem ec2-user@public-ip-of-the-ec2-instance "rm metadata.json;wget -q -O - http://169.254.169.254/latest/dynamic/instance-identity/document >> metadata.json;exit"
+scp -i path-to-privatekey.pem ec2-user@public-ip-of-the-ec2-instance:metadata.json .
 exit
